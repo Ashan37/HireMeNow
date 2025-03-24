e90a6fb21e3866dce234fe4ed138cc789c775b0a
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-03-24 12:28:51 +0530

    create mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 import express from "express";
 import cors from "cors";
-import 'dotenv/config';
-import mongoose from "mongoose";
-import cookieParser from "cookie-parser"
+import dotenv from "dotenv/config";
+import mongoose, { connect } from "mongoose";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
 
-const app= express();
-const port=process.env.PORT || 4000
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials: true}));
+app.use(cors({ credentials: true }));
 
-app.listen(port,()=>console.log(`Server started on PORT:${port}`));
+app.get('/', (req,res)=> res.send("API Working"));
+app.listen(port, ()=>console.log(`Server started on port:${port}`));
+
