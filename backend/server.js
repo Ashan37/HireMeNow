d17c94badfa9a581550b74c12738fd0221173ab9
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-03-26 10:03:00 +0530

    create authRouters

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv/config";
 import mongoose, { connect } from "mongoose";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
+import authRoutes from './routes/authRoutes.js'
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -14,5 +15,7 @@ app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
 app.get('/', (req,res)=> res.send("API Working"));
+
+
 app.listen(port, ()=>console.log(`Server started on port:${port}`));
 
