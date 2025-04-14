7839d029052a475177b264857e3a8927e093d329
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-04-14 15:37:10 +0530

    add footer and header to about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Navbarr from "../components/Navbarr";
+import Footer from "../components/Footer";
 
 export default function About() {
   return (
     <div>
-      
+       <Container-fluid>
+        <Navbarr />
+      </Container-fluid>
+
+      <Container-fluid>
+        <Footer />
+      </Container-fluid>
     </div>
   )
 }
