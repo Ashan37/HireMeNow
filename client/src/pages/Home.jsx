84bb397680165b7c07b997405159354b17a01a1e
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-04-23 12:59:23 +0530

    Add customer comment section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Footer from "../components/Footer";
 import Heroslider from "../components/Heroslider";
 import { Container, Row, Col } from "react-bootstrap";
 import "/src/styles/home.css";
+import CustomerComments from "../components/customerCommentSection";
 
 export default function Home() {
   return (
@@ -28,38 +29,35 @@ export default function Home() {
       </section>
 
       <Container className="job-section">
-  <Row>
-    <Col lg="6" md="6" sm="12" className="job-card seeker-card">
-      <h1 className="card-title">For Job Seekers</h1>
-      <div className="card-content">
-        <p>
-          Looking for your next big career move? Our platform connects job
-          seekers with top employers across industries. Browse hundreds of
-          verified job listings, filter by location, role, or experience
-          level, and apply in just a few clicks. Whether you're starting
-          fresh or leveling up, your next opportunity is just a search
-          away.
-        </p>
-      </div>
-    </Col>
-    <Col lg="6" md="6" sm="12" className="job-card employer-card">
-      <h1 className="card-title">For Employers</h1>
-      <div className="card-content">
-        <p>
-          Post jobs, find top talent, and streamline your hiring process
-          with our easy-to-use platform. Whether you're a startup or an
-          established company, reach qualified candidates actively looking
-          for roles in your industry. Create job listings, manage
-          applications, and connect with potential hires — all in one
-          place.
-        </p>
-      </div>
-    </Col>
-  </Row>
-</Container>
-
-
-     
+        <Row>
+          <Col lg="6" md="6" sm="12" className="job-card seeker-card">
+            <h1 className="card-title">For Job Seekers</h1>
+            <div className="card-content">
+              <p>
+                Looking for your next big career move? Our platform connects job
+                seekers with top employers across industries. Browse hundreds of
+                verified job listings, filter by location, role, or experience
+                level, and apply in just a few clicks. Whether you're starting
+                fresh or leveling up, your next opportunity is just a search
+                away.
+              </p>
+            </div>
+          </Col>
+          <Col lg="6" md="6" sm="12" className="job-card employer-card">
+            <h1 className="card-title">For Employers</h1>
+            <div className="card-content">
+              <p>
+                Post jobs, find top talent, and streamline your hiring process
+                with our easy-to-use platform. Whether you're a startup or an
+                established company, reach qualified candidates actively looking
+                for roles in your industry. Create job listings, manage
+                applications, and connect with potential hires — all in one
+                place.
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
 
       <section className="py-16 px-6 bg-white text-center">
         <h2 className="text-3xl font-bold mb-4">
@@ -128,6 +126,8 @@ export default function Home() {
           </Col>
         </Row>
       </Container>
+
+     <CustomerComments/>
 
       <Container-fluid>
         <Footer />
