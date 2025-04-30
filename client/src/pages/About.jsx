a7bd7d5969f619117aa353d49be80496b3d8e4b4
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-04-30 23:39:48 +0530

    add hero to contact page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbarr from "../components/Navbarr";
 import Footer from "../components/Footer";
-import HeroSliderAbout from '../components/HeroSliderAbout';
+import HeroSliderAbout from '../components/HeroSlider2';
 
 export default function About() {
   return (
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbarr from "../components/Navbarr";
 import Footer from "../components/Footer";
+import HeroSlider2 from '../components/HeroSlider2';
 
 export default function Contact() {
   return (
@@ -8,7 +9,7 @@ export default function Contact() {
        <Container-fluid>
         <Navbarr />
       </Container-fluid>
-
+      <HeroSlider2/>
       <Container-fluid>
         <Footer />
       </Container-fluid>
