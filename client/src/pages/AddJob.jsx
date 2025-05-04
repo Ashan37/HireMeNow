aa0b145d90b428d9873ab051ed2e3f268dd6e3a6
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-05-04 22:54:58 +0530

    updated

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Navbarr from "../components/Navbarr";
+import Footer from "../components/Footer";
 import axios from "axios";
 
 const AddJobVacancyForm = () => {
