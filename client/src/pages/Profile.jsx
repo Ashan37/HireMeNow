9abfb39891da666377a6522bd97be35ee79356fd
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-05-04 21:41:42 +0530

    add categories part to profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,11 +1,64 @@
 import React from 'react'
 import Navbarr from '../components/Navbarr'
 import Footer from '../components/Footer'
+import { Container,Row,Col } from 'react-bootstrap'
 
 export default function Profile() {
   return (
    <div>
     <Navbarr/>
+     {/* Categories part */}
+     <Container className="categories">
+        <div className="categories_head">
+          <h1>Job Categories</h1>
+        </div>
+
+        <Row className="categories_row">
+          <Col className="category_col">
+            <h4>Healthcare</h4>
+          </Col>
+          <Col className="category_col">
+            <h4>Information Technology</h4>
+          </Col>
+          <Col className="category_col">
+            <h4>Education</h4>
+          </Col>
+          <Col className="category_col">
+            <h4>Finance</h4>
+          </Col>
+        </Row>
+
+        <Row className="categories_row">
+          <Col className="category_col">
+            <h4>Hospitality & Tourism</h4>
+          </Col>
+          <Col className="category_col">
+            <h4>Engineering</h4>
+          </Col>
+          <Col className="category_col">
+            <h4>Construction</h4>
+          </Col>
+          <Col className="category_col">
+            <h4>Retail</h4>
+          </Col>
+        </Row>
+
+        <Row className="categories_row">
+          <Col className="category_col">
+            <h4>Marketing</h4>
+          </Col>
+          <Col className="category_col">
+            <h4>Manufacturing</h4>
+          </Col>
+          <Col className="category_col">
+            <h4>Legal</h4>
+          </Col>
+          <Col className="category_col">
+            <h4>Transportation & Logistics</h4>
+          </Col>
+        </Row>
+      </Container>
+
     <Footer/>
    </div>
   )
