a3039fc0047e7728284b4c68b4c57c084bd5cdcf
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-05-06 16:33:31 +0530

    create jobModel in backend

---
+++ backend/models/jobModel.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const jobSchema=new mongoose.Schema({
+    title: { type: String, required: true },
+    company: { type: String, required: true },
+    location: { type: String, required: true },
+    salary: { type: String, required: true },
+    type: { type: String, required: true },
+    description: { type: String, required: true },
+});
+
+const jobModel=mongoose.models.job||mongoose.model('job',jobSchema);
+
+export default jobModel;
